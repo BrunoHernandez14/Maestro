006bcbc944f0d7e43b2b89dff3c438f6a136da69
BrunoHernandez14 <brunohg04@gmail.com>
2025-01-09 19:12:20 -0600

    models

--- boundingBoxModel.py
+++ boundingBoxModel.py
@@ -1,13 +1,13 @@
 import cv2
 from ultralytics import YOLO
-model = YOLO("yolov8n-seg.pt")
+model = YOLO("yolov8n.pt")
 model.info()
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()  
     results = model.predict(source=frame, save=False, verbose=False)
     annotated_frame = results[0].plot() 
-    cv2.imshow("Segmentation Model", annotated_frame)
+    cv2.imshow("Object Detection Model", annotated_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         print("Exiting...")
         break
